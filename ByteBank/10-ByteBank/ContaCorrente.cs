56d66882830b75d3bb8198282f5866c393f2301f
Estagiario 015 <stag015@benner.com.br>
2019-05-23 10:40:11 -0300

    Exercicio 10 - Instancia

---
+++ ByteBank/10-ByteBank/ContaCorrente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_ByteBank
+{
+    class ContaCorrente
+    {
+        public int agencia;
+        public int conta;
+        public double saldo;
+        public Cliente titular;
+    }
+}
---
+++ ByteBank/10-ByteBank/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDoAndre = new ContaCorrente();
+            contaDoAndre.titular = new Cliente();
+
+            contaDoAndre.titular.nome = "Andre Huebes";
+        }
+    }
+}
