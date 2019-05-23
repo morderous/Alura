a44f9903ca19dab269d59463c89e23e1f98e4e11
Estagiario 015 <stag015@benner.com.br>
2019-05-23 10:30:37 -0300

    adc exercicio 9

---
+++ ByteBank/09-ByteBank/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p = new Pessoa();
+            p.nome = "Guilherme";
+            p.endereco.logradouro = "Avenida XYZ";
+
+            Console.WriteLine(p.endereco.logradouro);
+            Console.ReadLine();
+        }
+    }
+}
