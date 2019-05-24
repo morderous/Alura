6ce7bc9d9d205c1c33f50bc37765798e57882eeb
Estagiario 015 <stag015@benner.com.br>
2019-05-24 11:15:44 -0300

    adicionado comando .base

--- C#3_ByteBank/ByteBank/ByteBank/Funcionarios/Diretor.cs
+++ C#3_ByteBank/ByteBank/ByteBank/Funcionarios/Diretor.cs
@@ -10,8 +10,9 @@ namespace ByteBank.Funcionarios
     {
         public override double GetBonificacao()
         {
-            // Bonificação do diretor é outro salário
-            return Salario; 
+            // Bonificação do diretor é outro salário + a bonificação padrão dos funcionarios.
+            // base é utilizado para chamar atributos ou metodos de classes mães
+            return Salario + (base.GetBonificacao()); 
         }
     }
 }
