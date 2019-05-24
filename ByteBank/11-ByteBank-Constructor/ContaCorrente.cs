6a06b682e874d5fbf2754b85d3d47749aa4eda1b
Lucas-Note <lucashubes@gmail.com>
2019-05-24 00:05:29 -0300

    exercicio feito em casa

---
+++ ByteBank/11-ByteBank-Constructor/ContaCorrente.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_ByteBank_Constructor
+{
+    class ContaCorrente
+    {
+        public int Agencia { get; set; }
+        public int Conta { get; set; }
+        public double Saldo { get; set; }
+
+        public static int TotalDeContasCriadas { get; private set; }
+
+        public ContaCorrente(int ag, int numero, double saldo)
+        {
+            Agencia = ag;
+            Conta = numero;
+            Saldo = saldo;
+            TotalDeContasCriadas ++;
+        }
+        
+    }
+}
---
+++ ByteBank/11-ByteBank-Constructor/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_ByteBank_Constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaNum1 = new ContaCorrente(0121, 342509, 100.50);
+            Console.WriteLine("Contas Criadas até o momento:   " + ContaCorrente.TotalDeContasCriadas);
+            Console.WriteLine("Agencia: " + contaNum1.Agencia);
+            Console.WriteLine("C/C:     " + contaNum1.Conta);
+            Console.WriteLine("Saldo:   " + contaNum1.Saldo);
+            Console.WriteLine("------------------------------------------------------------------------");
+
+            ContaCorrente contaNum2 = new ContaCorrente(0099, 19994, 340.98);
+            Console.WriteLine("Contas Criadas até o momento:   " + ContaCorrente.TotalDeContasCriadas);
+            Console.WriteLine("Agencia: " + contaNum2.Agencia);
+            Console.WriteLine("C/C:     " + contaNum2.Conta);
+            Console.WriteLine("Saldo:   " + contaNum2.Saldo);
+            Console.WriteLine("------------------------------------------------------------------------");
+
+            ContaCorrente contaNum3 = new ContaCorrente(444, 000023, 9340.00);
+            Console.WriteLine("Contas Criadas até o momento:   " + ContaCorrente.TotalDeContasCriadas);
+            Console.WriteLine("Agencia: " + contaNum3.Agencia);
+            Console.WriteLine("C/C:     " + contaNum3.Conta);
+            Console.WriteLine("Saldo:   " + contaNum3.Saldo);
+
+            Console.ReadLine();
+        }
+    }
+}
