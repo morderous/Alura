457508a8d42cc84c2178137d959aef52f2bf3d84
Estagiario 015 <stag015@benner.com.br>
2019-05-30 09:25:07 -0300

    final curso c#4

--- C#4_ByteBank/ByteBank/LeitorDeArquivo.cs
+++ C#4_ByteBank/ByteBank/LeitorDeArquivo.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ByteBank
 {
-    class LeitorDeArquivo
+    class LeitorDeArquivo : IDisposable
     {
         public string Arquivo { get; }
         public LeitorDeArquivo(string arquivo)
+
         {
             Arquivo = arquivo;
+            //throw new FileNotFoundException();
             Console.WriteLine("Abrindo arquivo: " + arquivo);
         }
 
@@ -20,8 +23,7 @@ namespace ByteBank
             Console.WriteLine("Lendo linha...");
             return "Linha do arquivo";
         }
-
-        public void Fechar()
+        public void Dispose()
         {
             Console.WriteLine("Fechando Arquivo.");
         }
--- C#4_ByteBank/ByteBank/Program.cs
+++ C#4_ByteBank/ByteBank/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ByteBank
 {
@@ -10,62 +11,43 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
-            /*try
+            try
             {
-                ContaCorrente conta1 = new ContaCorrente(4564, 789684);
-                ContaCorrente conta2 = new ContaCorrente(7891, 456794);
-
-                conta1.Transferir(10000, conta2);
-                conta1.Sacar(10000);
+                CarregarContas();
+                Console.WriteLine("Execução finalizada. Tecle enter para sair");
+                Console.ReadLine();
             }
-            catch (OperacaoFinanceiraException e)
+            catch (Exception)
             {
-                Console.WriteLine(e.Message);
-                Console.WriteLine(e.StackTrace);
-
-                //Console.WriteLine("Informações da INNER EXCEPTION (exceção interna):");
-                
-            }*/
-
-            CarregarContas();
-            Console.WriteLine("Execução finalizada. Tecle enter para sair");
-            Console.ReadLine();
+                Console.WriteLine("CATCH NO METODO MAIN");
+                Console.ReadLine();
+            }
         }
         private static void CarregarContas()
         {
-            LeitorDeArquivo leitor = new LeitorDeArquivo("contas.txt");
 
-            leitor.LerProximaLinha();
-            leitor.LerProximaLinha();
-            leitor.LerProximaLinha();
-
-            leitor.Fechar();
-        }
-        // Teste com a cadeia de chamada:
-        // Metodo -> TestaDivisao -> Dividir
-        private static void Metodo()
-        {
-            TestaDivisao(0);
-        }
-
-        private static void TestaDivisao(int divisor)
-        {
-            int resultado = Dividir(10, divisor);
-            Console.WriteLine("Resultado da divisão de 10 por " + divisor + " é " + resultado);
-        }
-
-        private static int Dividir(int numero, int divisor)
-        {
-            try
+            using (LeitorDeArquivo leitor = new LeitorDeArquivo("teste.txt"))
             {
-                return numero / divisor;
+                leitor.LerProximaLinha();
             }
-            catch (DivideByZeroException)
-            {
-                Console.WriteLine("Exceção com numero=" + numero + " e divisor=" + divisor);
-                throw;
-            }
-        }
 
+            // -------------------------------------------------------
+            //LeitorDeArquivo leitor = null;
+            //try
+            //{
+            //    leitor = new LeitorDeArquivo("contas.txt");
+            //    leitor.LerProximaLinha();
+            //    leitor.LerProximaLinha();
+            //    leitor.LerProximaLinha();
+            //}
+            //finally
+            //{
+            //    Console.WriteLine("Executando o finally");
+            //    if (leitor != null)
+            //    {
+            //        leitor.Fechar();
+            //    }
+        }
     }
 }
+
