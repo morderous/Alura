9fe9ea106c231cd67aafe2b3019c88f2a63713f8
Lucas-Note <lucashubes@gmail.com>
2019-05-31 09:38:37 -0300

    pequenas alterações

--- Entity-Framework/Alura.Loja.Testes.ConsoleApp/Program.cs
+++ Entity-Framework/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -10,7 +10,36 @@ namespace Alura.Loja.Testes.ConsoleApp
     {
         static void Main(string[] args)
         {
+            using (var contexto = new LojaContext())
+            {
+                var produtos = contexto.Produtos.ToList();
+                foreach (var p in produtos)
+                {
+                    Console.WriteLine(p);
+                }
 
+                foreach(var e in contexto.ChangeTracker.Entries())
+                {
+                    Console.WriteLine(e.State);
+                }
+
+
+                //var p1 = produtos.First();
+                //p1.Nome = "Harry Potter";
+
+                //contexto.SaveChanges();
+
+                //Console.WriteLine("==============================");
+                //produtos = contexto.Produtos.ToList();
+                //foreach (var p in produtos)
+                //{
+                //    Console.WriteLine(p);
+                //}
+
+
+
+
+            }
 
             Console.WriteLine("feito");
             Console.ReadLine();
