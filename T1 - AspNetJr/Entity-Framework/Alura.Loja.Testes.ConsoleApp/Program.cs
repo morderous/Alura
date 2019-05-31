a98cbaaf2e7a2b6dcdb1ca8f10f0ffa1ff982005
Lucas-Note <lucashubes@gmail.com>
2019-05-31 11:42:23 -0300

    inicio da aula 4 e install do package-tools<migrations>

--- T1 - AspNetJr/Entity-Framework/Alura.Loja.Testes.ConsoleApp/Program.cs
+++ T1 - AspNetJr/Entity-Framework/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -11,44 +11,7 @@ namespace Alura.Loja.Testes.ConsoleApp
     {
         static void Main(string[] args)
         {
-            using (var contexto = new LojaContext())
-            {
-                var produtos = contexto.Produtos.ToList();
 
-                ExibeEntries(contexto.ChangeTracker.Entries());
-
-                var novoProduto = new Produto()
-                {
-                    Nome = "Kaiak",
-                    Categoria = "Perfume",
-                    Preco = 99.50
-                };
-                contexto.Produtos.Add(novoProduto);
-
-                ExibeEntries(contexto.ChangeTracker.Entries());
-
-                contexto.Produtos.Remove(novoProduto);
-
-                ExibeEntries(contexto.ChangeTracker.Entries());
-
-                //contexto.SaveChanges();
-
-                var entry = contexto.Entry(novoProduto);
-                Console.WriteLine("\n\n" + entry.Entity.ToString() + " - " + entry.State);
-
-            }
-
-            Console.WriteLine("                  ...feito...                ");
-            Console.ReadLine();
-        }
-
-        private static void ExibeEntries(IEnumerable<EntityEntry> entries)
-        {
-            Console.WriteLine("==============================");
-            foreach (var e in entries)
-            {
-                Console.WriteLine(e.Entity.ToString() + "-" + e.State);
-            }
         }
     }
 }
