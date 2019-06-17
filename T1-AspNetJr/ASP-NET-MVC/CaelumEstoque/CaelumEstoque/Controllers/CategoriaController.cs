c4d2ee47f32f7ac1d8cd60bf365099b454fa8c71
Lucas-Note <lucashubes@gmail.com>
2019-06-17 11:24:34 -0300

    CaelumEstoque criando views

---
+++ T1-AspNetJr/ASP-NET-MVC/CaelumEstoque/CaelumEstoque/Controllers/CategoriaController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CaelumEstoque.DAO;
+using CaelumEstoque.Models;
+
+namespace CaelumEstoque.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        // GET: Categoria
+        public ActionResult Index()
+        {
+            CategoriasDAO dao = new CategoriasDAO();
+            IList<CategoriaDoProduto> categoria = dao.Lista();
+            ViewBag.Categorias = categoria;
+            return View();
+        }
+    }
+}
