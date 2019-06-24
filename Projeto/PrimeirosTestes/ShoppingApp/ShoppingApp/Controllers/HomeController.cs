e90d05436d33572b55168bed7f2b85d2c2f25a4b
Lucas-Note <lucashubes@gmail.com>
2019-06-23 23:21:35 -0300

    primeiros passos do projeto

---
+++ Projeto/PrimeirosTestes/ShoppingApp/ShoppingApp/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using  ShoppingApp.Models;
+
+namespace ShoppingApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ShoppingDBEntities _db = new ShoppingDBEntities();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(_db.Lojas.ToList());
+        }
+
+        // GET: Home/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Home/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Home/Create
+        [HttpPost]
+        public ActionResult Create([Bind(Exclude = "Id")] Loja lojaToCreate)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            _db.Lojas.Add(lojaToCreate);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        // GET: Home/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var lojaToEdit = (from a in _db.Lojas
+                               where a.Id == id
+                               select a).First();
+            return View(lojaToEdit);
+        }
+
+        // POST: Home/Edit/5
+        [HttpPost]
+        public ActionResult Edit(Loja lojaToEdit)
+        {
+            var originalLoja = (from a in _db.Lojas
+                                where a.Id == lojaToEdit.Id
+                                select a).First();
+
+            if (!ModelState.IsValid)
+                return View(originalLoja);
+
+            _db.Entry(originalLoja).CurrentValues.SetValues(lojaToEdit);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+        
+    }
+}
